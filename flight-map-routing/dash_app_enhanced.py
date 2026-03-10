a131c2dcc2b2e2ea33abc0a4184c3e3b9bb2b5e5
AfiqDanial2003 <afiqdanial2003@yahoo.com>
2026-03-10 15:12:24 +0800

    Fixed the map viewing
    minimize the zoom out for user

--- flight-map-routing/dash_app_enhanced.py
+++ flight-map-routing/dash_app_enhanced.py
@@ -232,11 +232,16 @@ app.layout = html.Div([
                     id='flight-map',
                     center=[20, 0],
                     zoom=2,
-                    maxBounds=[[-90, -180], [90, 180]],
-                    maxBoundsViscosity=1.0,
+                    minZoom=2,
+                    maxZoom=8,
+                    bounceAtZoomLimits=False,
+                    preferCanvas=True,
+                    maxBounds=[[-85, -180], [85, 180]],
+                    maxBoundsViscosity=0.3,
                     worldCopyJump=False,
-                    style={'width': '100%', 'height': '520px'},
+                    style={'width': '100%', 'height': '420px', 'borderRadius': '18px'},
                     children=[
+                        dl.ScaleControl(position="bottomleft"),
                         dl.LayersControl([
     
                 # Normal Map
